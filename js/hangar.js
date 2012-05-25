711ce87dd8cbdc83b26e56503a75faf8cb5792b2
unknown <arghy@zmeu.(none)>
2012-05-26 01:10:53 +0300

    chat

---
+++ js/hangar.js
@@ -0,0 +1,22 @@
+/* Dragabble Weapons */
+
+$('.weapon').draggable({containment:'#hangar',revert:true,scroll:false});
+$( "#plane_holder" ).droppable({
+			drop: function( event, ui ) {
+				$(ui.draggable).hide("explode",1000);
+			
+				
+				var weapon_type = $(ui.draggable).data('type');
+				
+				if(weapon_type = 'radar') 
+								
+				
+				$('#weapon_info>ul>li').fadeIn(700);
+			}
+		});
+		
+$('.weapon_change').live("click", function (){
+	$(this).parent().fadeOut(400,$(this).parent().remove)
+	$()
+	
+});
--- js/script.js
+++ js/script.js
@@ -6,31 +6,13 @@ $(function(){
 
 
 /* =============================================================================
-Hangar Page
+Battle Page
 ========================================================================== */
 
-/* Dragabble Weapons */
-
-$('.weapon').draggable({containment:'#hangar',revert:true,scroll:false});
-$( "#plane_holder" ).droppable({
-			drop: function( event, ui ) {
-				$(ui.draggable).hide("explode",1000);
-			
-				
-				var weapon_type = $(ui.draggable).data('type');
-				
-				if(weapon_type = 'radar') 
-								
-				
-				$('#weapon_info>ul>li').fadeIn(700);
-			}
-		});
-		
-$('.weapon_change').live("click", function (){
-	$(this).parent().fadeOut(400,$(this).parent().remove)
-	$()
+$('#chat_close').click( function(){
+	$('#chat_slide').slideToggle(700)
 	
-})
+	});
 
 });
 
